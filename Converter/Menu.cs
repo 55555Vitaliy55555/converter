a78a262aba77c89d50317144ff771e8207ff5f67
St <vitaliy.sg.1111@gmail.com>
2017-09-24 19:09:59 +0300

    Добавлен прототип меню приложения

---
+++ Converter/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class Menu
+    {
+        char ans;
+        int choice;
+
+        public void Display()
+        {
+            Console.WriteLine("1 - перевод F -> C");
+            Console.WriteLine("2 - перевод C -> F");
+            Console.WriteLine("3 - Выход");
+        }
+    }
+}
--- Converter/Program.cs
+++ Converter/Program.cs
@@ -11,6 +11,9 @@ namespace Converter
         static void Main(string[] args)
         {
             Console.WriteLine("Конвертер единиц измерения");
+
+            Menu m = new Menu();
+            m.Display();
         }
     }
 }
